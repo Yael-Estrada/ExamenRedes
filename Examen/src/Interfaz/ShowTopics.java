6be33aec761558587fa77f18c4cc7c81d47c8ddf
Yael Estrada <yael_ex2.a97@hotmail.com>
2019-03-12 07:21:25 -0600

    Update ShowTopics.java

--- Examen/src/Interfaz/ShowTopics.java
+++ Examen/src/Interfaz/ShowTopics.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Interfaz;
 
+import BDD.objectQuery.publicacion;
+import java.util.Vector;
 import javax.swing.ImageIcon;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -15,6 +13,7 @@ public class ShowTopics extends javax.swing.JFrame {
     String username;
     DefaultTreeModel modelo;
     DefaultTreeCellRenderer render;
+    Vector<publicacion> publicaciones;
     public ShowTopics(String user) {
         initComponents();
         this.username=user;
@@ -25,24 +24,39 @@ public class ShowTopics extends javax.swing.JFrame {
         render.setLeafIcon(new ImageIcon("C:\\Users\\Principal\\Documents\\GitHub\\ExamenRedes\\Examen\\iconos\\file.png"));
     }
 
+    public void setPublicaciones(Vector<publicacion> p){
+        publicaciones=p;
+        int ind=0;
+        for(publicacion s:publicaciones){
+            modelo.insertNodeInto(new DefaultMutableTreeNode(s.getTemaPublicacion()),(DefaultMutableTreeNode)modelo.getRoot(), ind++);
+        }
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         indice = new javax.swing.JTree();
+        openTopic = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jScrollPane1.setViewportView(indice);
+
+        openTopic.setText("Abrir");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 347, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 347, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(162, 162, 162)
+                        .addComponent(openTopic)))
                 .addContainerGap(25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -50,7 +64,9 @@ public class ShowTopics extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(30, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43))
+                .addGap(9, 9, 9)
+                .addComponent(openTopic)
+                .addContainerGap())
         );
 
         pack();
@@ -69,5 +85,6 @@ public class ShowTopics extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTree indice;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton openTopic;
     // End of variables declaration//GEN-END:variables
 }
