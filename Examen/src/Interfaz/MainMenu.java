4b5574aef882dbe3232f4c4bc2a4892910b0930a
Yael Estrada <yael_ex2.a97@hotmail.com>
2019-03-13 21:42:07 -0600

    Update MainMenu.java

--- Examen/src/Interfaz/MainMenu.java
+++ Examen/src/Interfaz/MainMenu.java
@@ -1,6 +1,18 @@
 
 package Interfaz;
 
+import BDD.objectQuery;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -15,6 +27,8 @@ public class MainMenu extends javax.swing.JFrame {
         initComponents();
         this.username=user;
         DefaultMutableTreeNode raiz=new DefaultMutableTreeNode("Temas de interés");
+        raiz.add(new DefaultMutableTreeNode("Electronica"));
+        raiz.add(new DefaultMutableTreeNode("Redes"));
         modelo = new DefaultTreeModel(raiz);
         indice.setModel(modelo);
         render= (DefaultTreeCellRenderer)indice.getCellRenderer();
@@ -30,10 +44,17 @@ public class MainMenu extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         indice = new javax.swing.JTree();
         jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        textPost = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         createPost.setText("Crear Publicacion");
+        createPost.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createPostActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Buscar");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -46,14 +67,14 @@ public class MainMenu extends javax.swing.JFrame {
 
         jLabel1.setText("Temas");
 
+        textPost.setColumns(20);
+        textPost.setRows(5);
+        jScrollPane2.setViewportView(textPost);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addComponent(createPost)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addComponent(jScrollPane1)
@@ -64,17 +85,24 @@ public class MainMenu extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 255, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addGap(124, 124, 124))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(createPost)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jButton2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(createPost)
                 .addContainerGap())
         );
@@ -86,6 +114,44 @@ public class MainMenu extends javax.swing.JFrame {
         Search s=new Search(username);
         s.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void createPostActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createPostActionPerformed
+        try {
+            String texto=textPost.getText();
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)indice.getLastSelectedPathComponent();
+            if (node == null) return;
+            Object nodeInfo = node.getUserObject();
+            String tema=(String)nodeInfo;
+            Socket cl=new Socket("127.0.0.1",1234);
+            PrintWriter pw=new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+            String eco="Publicar";
+            pw.println(eco);
+            pw.flush();
+            pw.close();
+            cl.close();
+            
+            cl=new Socket("127.0.0.1",1234);
+            pw=new PrintWriter(new OutputStreamWriter(cl.getOutputStream()));
+            pw.println(tema);
+            pw.flush();
+            pw.println(texto);
+            pw.flush();
+            pw.println(username);
+            pw.flush();
+            pw.close();
+            cl.close();
+            
+            cl=new Socket("127.0.0.1",1234);
+            BufferedReader br=new BufferedReader(new InputStreamReader(cl.getInputStream()));
+            String msj=br.readLine();
+            System.out.println(""+msj);
+            br.close();
+            cl.close();
+            return;
+        } catch (IOException ex) {
+            Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_createPostActionPerformed
 
     public static void main(String args[]) {
 
@@ -102,5 +168,7 @@ public class MainMenu extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea textPost;
     // End of variables declaration//GEN-END:variables
 }
